691f780b2e44c2433aa8a974f1eb4ea6a983196f
1324589321@qq.com <1324589321@qq.com>
2023-02-23 10:02:10 +0800

    test: 动态加载

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,15 @@ module.exports = {
     // parserOptions: {
     //     parser: 'babel-eslint'
     // },
+    root: true, //默认情况下，ESLint 会在所有父级目录里寻找配置文件，一直到根目录。如果发现配置文件中有 “root”: true，它就会停止在父级目录中寻找。
     env: {
         es6: true,
         browser: true, // 
         node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module'
     },
     "extends": [
         "eslint:recommended"
---
+++ demo.js
@@ -0,0 +1,2 @@
+let b = (p) => p + 1;
+export default b;
